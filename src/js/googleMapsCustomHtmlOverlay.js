fb2786f7937d70d356c71764efe6e7a17962f1a9
Danny Chmylov <chmylov@outlook.com>
2018-06-02 20:13:36 +0200

    Added debug mode

--- src/js/googleMapsCustomHtmlOverlay.js
+++ src/js/googleMapsCustomHtmlOverlay.js
@@ -4,32 +4,74 @@ function HtmlOverlay(options) {
   this.html = options.html;
   this.divClass = options.divClass;
   this.align = options.align;
+  this.isDebugMode = options.debugMode;
   this.setMap(options.map);
 
   this.ifNotUndefined = function(arg, callback) {
     if (typeof arg === 'undefined') return;
     return callback();
   };
+
+  this.isBoolean = arg => {
+    if (typeof arg === 'boolean') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.isNotUndefined = arg => {
+    if (typeof arg !== 'undefined') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.hasContent = arg => {
+    console.log(arg.length);
+    if (arg.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
 
 HtmlOverlay.prototype = new google.maps.OverlayView();
 
 HtmlOverlay.prototype.onAdd = function() {
-  // Create the div element.
+  // Create div element.
   this.div = document.createElement('div');
   this.div.style.position = 'absolute';
 
   // Validate and set custom div class
-  this.ifNotUndefined(this.divClass, () =>
-    this.div.classList.add(this.divClass)
-  );
+  if (this.isNotUndefined(this.divClass) && this.hasContent(this.divClass))
+    this.div.className = this.divClass;
 
   // Validate and set custom HTML
-  this.ifNotUndefined(this.html, () => {
+  if (this.isNotUndefined(this.html) && this.hasContent(this.html))
     this.div.innerHTML = this.html;
-  });
 
-  // Add the element to the "overlayMouseTarget" pane.
+  // If debug mode is enabled custom content will be replaced with debug content
+  if (this.isBoolean(this.isDebugMode) && this.isDebugMode) {
+    this.div.className = 'debug-mode';
+    this.div.innerHTML =
+      '<div style="height: 10px; width: 10px; background: red; border-radius: 100%;"></div>' +
+      '<div style="position: absolute; top: 5px; padding: 5px; width: 130px; text-align: center; font-size: 18px; text-transform: uppercase; font-weight: bolder; background: red; color: white; font-family: Arial;">Debug mode</div>';
+    this.div.setAttribute(
+      'style',
+      'position: absolute;' +
+        'border: 5px dashed red;' +
+        'height: 150px;' +
+        'width: 150px;' +
+        'display: flex;' +
+        'justify-content: center;' +
+        'align-items: center;'
+    );
+  }
+
+  // Add element to the "overlayMouseTarget" pane.
   this.getPanes().overlayMouseTarget.appendChild(this.div);
 
   // Add listener to the element.
